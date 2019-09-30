686acb9532cac0e839ccfce2e86385c04e732b03
umekow <umekolashun@gmail.com>
2019-09-30 14:34:34 -0500

    created a todo class component

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Todo extends React.Component{
+    constructor(){
+        super();
+        this.state = []
+    }
+
+    render() {
+        return(
+            <>
+                <h1>This is the todo component</h1>
+            </>
+        )
+    }
+}
+
+export default Todo
